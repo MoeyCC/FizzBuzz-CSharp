de48b3fb3ebf58e34524fbc3c5d12f811f8e654e
Mohan Ratnam <MoeyCC>
2023-10-11 12:02:45 +0100

    Modified 1. Allow user to enter max number 2. Allow program to run indefinitely unless user enters Exit 3. Validate user entry to only allow numbers or Exit

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,67 +10,81 @@ internal class Program
 
         List<string> result = new List<string>();
 
-        #nullable disable
+#nullable disable
 
-        Console.WriteLine("Please enter a maximum number: ");
-        int maxNumber = int.Parse(Console.ReadLine());
-
-        for (int i = 1; i <= maxNumber; i++)
+        while (true)
         {
-            result.Clear();
-            if (IsDivisibleBy(i, 3)) result.Add("Fizz");
+            Console.Write("Please enter a maximum number: ");
+            string input = Console.ReadLine();
 
-            if (IsDivisibleBy(i, 5)) result.Add("Buzz");
+            CheckExitProgram(input);
 
-            if (IsDivisibleBy(i, 7)) result.Add("Bang");
-
-            if (IsDivisibleBy(i, 11))
+            int count;
+            int.TryParse(input, out count);
+            if (count != 0)
             {
-                result.Clear();
-                result.Add("Bong");
-            }
-
-            if (IsDivisibleBy(i, 13))
-            {
-                if (result.Count > 0)
+                for (int i = 1; i <= count; i++)
                 {
-                    int position = IsFoundLetter(result, "B");
-                    if(position == -1){
-                        result.Add("Fezz");
-                    } else result.Insert(position, "Fezz");
-                }
-                else {
                     result.Clear();
-                    result.Add("Fezz");
+                    if (IsDivisibleBy(i, 3)) result.Add("Fizz");
+
+                    if (IsDivisibleBy(i, 5)) result.Add("Buzz");
+
+                    if (IsDivisibleBy(i, 7)) result.Add("Bang");
+
+                    if (IsDivisibleBy(i, 11))
+                    {
+                        result.Clear();
+                        result.Add("Bong");
+                    }
+
+                    if (IsDivisibleBy(i, 13))
+                    {
+                        if (result.Count > 0)
+                        {
+                            int position = IsFoundLetter(result, "B");
+                            if (position == -1) result.Add("Fezz");
+                            else result.Insert(position, "Fezz");
+                        }
+                        else
+                        {
+                            result.Clear();
+                            result.Add("Fezz");
+                        }
+                    }
+
+                    if (IsDivisibleBy(i, 17)) result.Reverse();
+
+                    if (result.Count == 0) result.Add(i.ToString());
+
+                    Console.WriteLine(string.Join("", result));
                 }
             }
-
-            if (IsDivisibleBy(i, 17)) result.Reverse();
-
-            if (result.Count == 0) result.Add(i.ToString());
-
-            Console.WriteLine(string.Join("", result));
         }
-
-    } 
+    }
 
     private static bool IsDivisibleBy(int numerator, int denominator)
     {
         return numerator % denominator == 0;
-    } 
+    }
 
     private static int IsFoundLetter(List<string> message, string letter)
     {
-        int result = -1; 
+        int result = -1;
         for (int i = 0; i < message.Count(); i++)
         {
-            if(message[i][0].ToString() == "B")
+            if (message[i][0].ToString() == "B")
             {
                 result = i;
                 break;
-            }   
+            }
         }
         return result;
+    }
+
+    private static void CheckExitProgram(string input)
+    {
+        if(input == "Exit") Environment.Exit(0);
     }
 
     static void Main(string[] args)
